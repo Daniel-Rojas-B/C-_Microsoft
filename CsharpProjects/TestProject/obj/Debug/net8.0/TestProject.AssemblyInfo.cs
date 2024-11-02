49ef4197eb5dcd73bc712b4265c42bac2a284127
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-11-02 13:24:34 -0700

    Added MSSA code

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f85c7ee84f39523d6624c0c114644b5c07bc8a79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a0d5fec53b64fd758799a4bc5071283df1bba8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
